f3e8c4c8c52abc3166a6c1734f03f9d6d9585126
Desarrollo <desarrollo@Mac-mini-de-Desarrollo.local>
2019-05-17 17:50:13 -0500

    Menu vertical

--- src/app/Components/MenuH.js
+++ src/app/Components/MenuH.js
@@ -4,7 +4,17 @@ import React, {Component} from 'react';
 class MenuH extends Component{
     render(){
         return(
-            <h1>Hola menuh</h1>
+            <div className="menuh">
+                <div className="logo"><img src="assets/img/roliagroup_logo.png" /></div>
+                <ul>
+                    <li><a href=""></a>Home</li>
+                    <li><a href=""></a>Customers</li>
+                    <li><a href=""></a>Workers</li>
+                    <li><a href=""></a>Services</li>
+                    <li><a href=""></a>Actives</li>
+                    <li><a href=""></a>Settings</li>
+                </ul>
+            </div>
         )
     }
 }
